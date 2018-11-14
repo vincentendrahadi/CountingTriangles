3f92bec2c4dc250cfcd5491787a772a962439881
andika <dika.kusuma23@gmail.com>
2018-11-14 20:55:25 +0700

    Update remove duplicate using set

--- src/TriangleCounter.java
+++ src/TriangleCounter.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.lang.Integer;
-import java.util.Iterator;
+import java.util.*;
 import java.lang.Long;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -14,7 +14,45 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TriangleCounter {
+  
+  // public static class DuplicateMapper
+  //   extends Mapper<Object, Text, Text, LongWritable>{
 
+  //     public void map(Object key, Text value, Context context)
+  //       throws IOException, InterruptedException {
+
+  //         String[] user_follower = value.toString().trim().split("\\s+");
+
+  //         if (user_follower.length > 1) {
+  //           Long userID = Long.parseLong(user_follower[0]);
+  //           Long followerID = Long.parseLong(user_follower[1]);
+  //           Text key = new Text();
+  //           private final LongWritable dummy = new LongWritable(-1);
+
+  //           if (userID < followerID) {
+  //             key = user_follower[0] + "," + user_follower[1];
+  //           } else {
+  //             key = user_follower[1] + "," + user_follower[0];
+  //           }
+
+  //           context.write(key, dummy);
+  //         }
+  //       }
+  //   }
+
+  // public static class DuplicateReducer
+  //   extends Reducer<Text, LongWritable, LongWritable, LongWritable> {
+
+  //     public void reduce(Text key, Iterable<LongWritable> values,
+  //       Context context) throws IOException, InterruptedException {
+  //         String[] nodes = key.toString().trim().split(",");
+
+  //         LongWritable key = new LongWritable(Long.parseLong(nodes[0]));
+  //         LongWritable value = new LongWritable(Long.parseLong(nodes[1]));
+
+  //         context.write(key, value);
+  //       }
+  //   }
 
   public static class PairMapper
        extends Mapper<Object, Text, LongWritable, LongWritable>{
@@ -44,34 +82,46 @@ public class TriangleCounter {
     private final LongWritable dollar = new LongWritable(-1);
 
     public void reduce(LongWritable key, Iterable<LongWritable> values,
-        Context context) throws IOException, InterruptedException {
-
-
-      // TODO : remove duplicate in values
-            
+      Context context) throws IOException, InterruptedException {
+        
+      
       Iterator<LongWritable> itr = values.iterator();
-      Integer counter = 0;
       Text newKey = new Text();
-      Text pairKey = new Text();
-      String temp = "";
-
-
-      // TODO : Generate all possible combinations
+      Set<Long> set = new LinkedHashSet<>();
       while (itr.hasNext()) {
-        counter++;
-        String value = Long.toString(itr.next().get());
+        // context.write(new Text(key.toString()), new LongWritable(itr.next().get()));
+        set.add(itr.next().get());
+      }
+      ArrayList<Long> arrayList = new ArrayList<>();
+      Iterator<Long> iter = set.iterator();
+      while (iter.hasNext()) {
+        Long item = new Long(iter.next());
+        arrayList.add(item);
+      }
+
+      for (int i = 0; i < arrayList.size(); i++) {
+        Long value1 = arrayList.get(i);
+        String strValue1 = value1.toString();
         
         // Emit read key value pair
-        newKey.set(key.toString() + ',' + value);
+        // (key, value1), $
+        newKey.set(key.toString() + ',' + strValue1);
         context.write(newKey, dollar);
         
-        // Emit triad from read key value pair
-        if (counter == 2 ) {
-          counter = 0;
-          pairKey.set(temp + value);
-          context.write(pairKey, key);
-        } else {
-          temp = value + ',';
+        for (int j = i + 1; j < arrayList.size(); j++) {
+          Long value2 = arrayList.get(j);
+          String strValue2 = value2.toString();
+          // Emit triad from read key value pair
+          newKey.set(strValue1 + "," + strValue2);
+          int compare = value1.compareTo(value2);
+          // context.write(newKey, new LongWritable());
+          if (compare < 0) {
+            newKey.set(strValue1 + "," + strValue2);
+            context.write(newKey, key);
+          } else {
+            newKey.set(strValue2 + "," + strValue1);
+            context.write(newKey, key);
+          }
         }
       }
     }
@@ -94,8 +144,8 @@ public class TriangleCounter {
   public static class TriangleReducer
     extends Reducer<Text, LongWritable, Text, LongWritable> {
     
-    long count = 0;
-    boolean hasDollar = false;
+    long count;
+    boolean hasDollar;
 
     public void reduce(Text key, Iterable<LongWritable> values,
         Context context) throws IOException, InterruptedException {
@@ -103,6 +153,8 @@ public class TriangleCounter {
       final Text RESULT_STRING = new Text("result");
       
       Iterator<LongWritable> itr = values.iterator();
+      count = 0;
+      hasDollar = false;
       
       while (itr.hasNext()) {
         Long value = itr.next().get();
